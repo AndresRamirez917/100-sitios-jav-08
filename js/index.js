4dd88c66a21cac3b7999eedc165afd96111a323b
AndresRamirez917 <andresmintic2021@gmail.com>
2024-08-31 13:44:17 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -22,14 +22,22 @@ async function getData(){
                 `)
                 const services_row = document.querySelector('.services-row');
                 services_row.append(card)
-        }for(element.id == 1; element.id <= 1; element.id++){
+
+            }
+
+            // prueba para recorrer de otra manera el array para "pintar"
+            // un template html (rotating-text-wrapper) sin SALIR DE LA 
+            // FUNCION PRINCIPAL (getData()) Y USANDO LOS DATOS OBTENIDOS  
+            // (ELEMENT) POR EL FOREACH PRINCIPAL
+        
+        for(element.id == 1; element.id <= 1; element.id++){
 
             const text_animation = document.createRange().createContextualFragment(`
                 
                  <div class="rotating-text-wrapper">
-                    <h2>${element.name}</h2>
-                    <h2>${element.origin.name}</h2>
-                    <h2>${element.status}</h2>
+                    <h2>Nombre: ${element.name}</h2>
+                    <h2>Origen: ${element.origin.name}</h2>
+                    <h2>Estatus: ${element.status}</h2>
                   </div>
 
                 `)
@@ -38,6 +46,45 @@ async function getData(){
             container.append(text_animation)
         }
     });
+}
+
+const btn_enviar = document.getElementById('btn-enviar')
+
+const valida = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje')
+
+    if(nombre.value == ""){
+        alert("El campo nombre no puede estar vacío")
+        return false;
+    }
+
+    if(email.value == ""){
+        alert("El campo email no puede estar vacío")
+        return false;
+    }
+
+    if(mensaje.value == ""){
+        alert("El campo mensaje no puede estar vacío")
+        return false;
+    }
+
+    if(!email_valido(email.value)){
+        alert("El formato de correo no es válido")
+        return false;
+    }
+    nombre.value = "";
+    email.value = "";
+    mensaje.value = ""
+    return true;
+}
+
+btn_enviar.addEventListener("click", valida)
+
+const email_valido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
 getData()
